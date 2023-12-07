84c4b2e4541e061519c999d8d6f768a8b5944feb
Reetika <chauhanreetika45@gmail.com>
2023-12-06 16:08:30 -0800

    implemented pagination

--- imagegeneration.js
+++ imagegeneration.js
@@ -1,53 +1,114 @@
-//import { createClient } from 'pexels';
 const generateform = document.querySelectorAll(".generate-form")[0];
 console.log(generateform)
 const imagecards = document.querySelectorAll(".img-card");
-
-console.log(imagecards)
-
+const loginEle = document.getElementById("login");
+const logoutEle = document.getElementById("logout")
+const prev=document.querySelector(".prev");
+const next=document.querySelector(".next");
+const page=document.querySelector(".page-num");
+const paginationdiv = document.querySelectorAll(".pagination")[0]
+// pagination
+const maxItem=8;
+let index=1;
 const API_KEY = ''
 //const client = createClient(API_KEY);
-
 generateform.addEventListener("submit",(e) =>{
     e.preventDefault();
-    console.log("event ",e)
+    index=1;
     const usersearchtext = e.srcElement[0].value;
-    console.log("text",usersearchtext)
     const numberofimages = parseInt(e.srcElement[1].value);
-    console.log('images number',numberofimages)
-    console.log("i am working")
     const imgquantity = document.querySelectorAll(".img-quantity")[0];
-    console.log(imgquantity.value);
-    console.log(imagecards.length)
+    if (numberofimages == 0){
+        for(let i=0;i < imagecards.length;i++){
+            imagecards[i].classList.add('img-card')
+            imagecards[i].classList.remove('hide-img');
+            let imgcard =imagecards[i].getElementsByTagName("img")[0]
+            imgcard.setAttribute("src", 'images/loading-bar.png') 
+            imagecards[i].classList.add("loading")
+        }
+        checkfunc()
+        showfunc()
+        paginationfunc()
+    }
+    else{
+    paginationdiv.classList.add("hide-img")
     for(let i=0;i < imagecards.length;i++){
-            console.log(i)
-            if (i < imgquantity.value){
-                console.log(imagecards[i])
+            if (i < numberofimages){
                 imagecards[i].classList.add('img-card')
                 imagecards[i].classList.remove('hide-img');
                 let imgcard =imagecards[i].getElementsByTagName("img")[0]
-                console.log(imgcard)
                 imgcard.setAttribute("src", 'images/loading-bar.png') 
                 imagecards[i].classList.add("loading")
                 
             }
             else{
-                console.log("helllll")
                 imagecards[i].classList.remove('img-card')
                 imagecards[i].classList.add('hide-img');
+                imagecards[i].classList.remove("show");
             }
-
+    }
+    
+    
     }
     handleimagegeneration(usersearchtext,numberofimages)
-    
-
 
 })
+
+const paginationfunc = function(){
+    paginationdiv.classList.remove("hide-img")
+    prev.addEventListener("click",function(){
+        index = 1;
+        console.log("index decre",index)
+        checkfunc()
+        showfunc();
+
+    })
+    next.addEventListener("click",function(){
+        index = 2;
+        console.log("index incre",index)
+        checkfunc();
+        showfunc();  
+    })
+
+}
+
+const checkfunc = function(){
+    const pagination=Math.ceil(imagecards.length/maxItem);
+    if(index>=pagination){
+        next.classList.add("disabled");
+    }
+    else{
+    next.classList.remove("disabled");	
+    }
+
+    if(index<=1){
+        prev.classList.add("disabled");
+    }
+    else{
+    prev.classList.remove("disabled");	
+    }
+}
+
+const showfunc = function(){
+    for (let i = 0 ;i < imagecards.length; i++){
+        imagecards[i].classList.remove("show");
+        imagecards[i].classList.remove("img-card");
+        imagecards[i].classList.add("hide-img");
+        if(i>=(index*maxItem)-maxItem && i<index*maxItem){
+            imagecards[i].classList.remove("hide-img");
+            imagecards[i].classList.add("img-card");
+            imagecards[i].classList.add("show");
+        }
+       
+}
+page.innerHTML=index;
+                
+}
 
 // Creating API request to 
 const handleimagegeneration = function(usersearchtext,numberofimages){
     try {
-   fetch( `https://api.pexels.com/v1/search?query=${usersearchtext}?per_page=${15}`,{
+   fetch( `https://api.pexels.com/v1/search?query=${usersearchtext}?per_page=${10}`,{
         headers:{
             Authorization:API_KEY
         }
@@ -56,7 +117,6 @@ const handleimagegeneration = function(usersearchtext,numberofimages){
         return resp.json()
      })
      .then(data =>{
-        console.log('pictures',data.photos)
         updateimages(data.photos);
      })
     } catch(error){
@@ -65,12 +125,15 @@ const handleimagegeneration = function(usersearchtext,numberofimages){
 }
 
 const updateimages = function(images){
+   
     for(let i = 0; i < imagecards.length;i++ ){
-        if(imagecards[i].classList.contains("img-card")){
+       
             let imgcard =imagecards[i].getElementsByTagName("img")[0]
             imagecards[i].classList.remove("loading")
             imgcard.setAttribute("src",images[i].src.tiny)
-        }
-
+        
     }
+ 
+
 } 
+
--- signInform.js
+++ signInform.js
@@ -1,5 +1,5 @@
 const forminfo = document.getElementById('personalinfo')
-
+let username = ''
 console.log("hiiii")
 forminfo.addEventListener("submit",(e)=>{
     e.preventDefault()
@@ -7,15 +7,21 @@ forminfo.addEventListener("submit",(e)=>{
     console.log(fullname)
     const emailEle = document.getElementById("Email").value
     const passwordEle = document.getElementById("Password").value
-    const username = document.getElementById("username").value
+    username = document.getElementById("username").value
     const personalinfo = {
         name : fullname,
         email:emailEle,
         passwordEle: passwordEle,
+        SignIn: true
     }
     console.log("hello")
+    sessionStorage.setItem('username',username)
     localStorage.setItem(username,JSON.stringify(personalinfo))
     forminfo.submit()
 })
+const getusername = function (){
+    console.log("in signin username",username)
+    return username
+}
 
 
